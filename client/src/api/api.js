9acbf260141e5a9d116a4307d4f9e228c640590f
sheor <sheorana065@gmail.com>
2023-11-30 21:21:02 +0530

    changed api

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -14,7 +14,7 @@ export const getAllProducts = async () => {
 
 export const getProductById = async (id) => {
   try {
-    const response = await axios.get(`${baseURL}/products/${id}`);
+    const response = await axios.get(`${baseURL}/api/products/${id}`);
     return response.data;
   } catch (error) {
     console.error(`Error fetching product with ID ${id}:`, error.message);
